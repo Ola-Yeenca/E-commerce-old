8fbc54165b470966cae78265f5b6e084b122c36c
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-07-18 02:30:12 +0200

    admin and backend creation

--- shopify/shopify/urls.py
+++ shopify/shopify/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from core.views import index, contact
 
@@ -6,4 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
