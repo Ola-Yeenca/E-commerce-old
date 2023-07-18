38774638898c5b0a34c43700418eef2cae5968cf
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-07-18 03:35:19 +0200

    accessories details

--- shopify/shopify/settings.py
+++ shopify/shopify/settings.py
@@ -108,8 +108,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
-    ]
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 # Media settings
 MEDIA_URL = '/media/'
--- shopify/shopify/urls.py
+++ shopify/shopify/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from core.views import index, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
