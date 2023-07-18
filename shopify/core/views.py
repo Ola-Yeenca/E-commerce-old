cb3b9eac688698fc35ce833e2dfce170dee04e68
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-07-18 16:03:35 +0200

    fixed signup and redirecting page

--- shopify/core/views.py
+++ shopify/core/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from item.models import  Category, Item
+from django.contrib.auth import login as auth_login
+
+from item.models import Category, Item
 from .forms import SignupForm
 
 # Create your views here.
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
-    return render(request, 'core/index.html', context = {
+    return render(request, 'core/index.html', context={
         'items': items,
         'categories': categories
     })
@@ -18,13 +20,12 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save() # save the user
-            return redirect('core:index')
+            user = form.save()  # Save the user
+            auth_login(request, user)  # Log in the user after signup
+            return redirect('core:index')  # Redirect to the main page
     else:
         form = SignupForm()
-    return render(request, 'core/signup.html', context = {
-        'form': form
-    })
+    return render(request, 'core/signup.html', context={'form': form})
 
-def login(request):
+def login_view(request):
     return render(request, 'core/login.html')
