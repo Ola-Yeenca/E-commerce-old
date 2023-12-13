b468ed7a43c9396857e510de490a9616e5b5c46e
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-12-13 19:57:07 +0100

    build and yaml file

--- shopify/communication/routing.py
+++
@@ -1,8 +0,0 @@
-# communication/routing.py
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/conversation/(?P<conversation_pk>\d+)/$', consumers.MessageConsumer.as_asgi()),
-]
--- shopify/core/static/javascript/application.js
+++
@@ -1,6 +0,0 @@
-// Reset search founction
-function clearFilters() {
-  console.log("Actived!");
-  // Clear the filters and reset the form fields
-  document.getElementById("filter-form").reset();
-}
